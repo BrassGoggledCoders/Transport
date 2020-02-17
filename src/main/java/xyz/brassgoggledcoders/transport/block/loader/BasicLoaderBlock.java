efe58bcdf5620e75a36d34f1e72eefe0970b2226
Skylar <skylar.som@gmail.com>
2020-02-17 17:46:42 -0500

    Nuked Loader code from Orbit
    Will return it after getting rails/carts working.

--- src/main/java/xyz/brassgoggledcoders/transport/block/loader/BasicLoaderBlock.java
+++ src/main/java/xyz/brassgoggledcoders/transport/block/loader/BasicLoaderBlock.java
@@ -1,11 +1,39 @@
 package xyz.brassgoggledcoders.transport.block.loader;
 
-import com.teamacronymcoders.base.blocks.BlockSidedBase;
-import net.minecraft.block.material.Material;
-import xyz.brassgoggledcoders.transport.tileentity.loader.TileEntityLoaderBase;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
-public abstract class BasicLoaderBlock<T extends TileEntityLoaderBase> extends BlockSidedBase<T> {
-    protected BasicLoaderBlock(String name) {
-        super(Material.IRON, name + "_loader");
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public abstract class BasicLoaderBlock extends Block {
+    protected BasicLoaderBlock(Block.Properties properties) {
+        super(properties);
     }
+
+    @Override
+    @Nonnull
+    @SuppressWarnings("deprecation")
+    public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player,
+                                             Hand hand, BlockRayTraceResult rayTraceResult) {
+
+        return ActionResultType.PASS;
+    }
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public abstract TileEntity createTileEntity(BlockState state, IBlockReader world);
 }
