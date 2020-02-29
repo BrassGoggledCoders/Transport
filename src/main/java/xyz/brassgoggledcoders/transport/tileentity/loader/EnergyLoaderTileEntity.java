dfedbf66da766ff6ef531bced1b004f5e5bb9ba8
Skylar <skylar.som@gmail.com>
2020-02-29 15:16:10 -0500

    Creating Energy/Fluid Loaders

--- src/main/java/xyz/brassgoggledcoders/transport/tileentity/loader/EnergyLoaderTileEntity.java
+++ src/main/java/xyz/brassgoggledcoders/transport/tileentity/loader/EnergyLoaderTileEntity.java
@@ -2,40 +2,69 @@ package xyz.brassgoggledcoders.transport.tileentity.loader;
 
 import com.hrznstudio.titanium.api.IFactory;
 import com.hrznstudio.titanium.api.client.IScreenAddon;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.IEnergyStorage;
+import xyz.brassgoggledcoders.transport.capability.EnergyComponent;
+import xyz.brassgoggledcoders.transport.capability.EnergyStorageDirectional;
 import xyz.brassgoggledcoders.transport.content.TransportBlocks;
 
+import java.util.Collections;
 import java.util.List;
 
 public class EnergyLoaderTileEntity extends BasicLoaderTileEntity<IEnergyStorage> {
+    private final EnergyComponent energyComponent;
+    private final LazyOptional<IEnergyStorage> lazyEnergy;
+
     public EnergyLoaderTileEntity() {
         super(TransportBlocks.ENERGY_LOADER.getTileEntityType(), CapabilityEnergy.ENERGY);
+        this.energyComponent = new EnergyComponent(10000, 50, 15);
+        this.lazyEnergy = LazyOptional.of(() -> energyComponent);
+
     }
 
     @Override
     protected void transfer(IEnergyStorage from, IEnergyStorage to) {
-
+        int amountSimPulled = from.extractEnergy(10000, true);
+        if (amountSimPulled > 0) {
+            int amountSimPushed = to.receiveEnergy(amountSimPulled, true);
+            if (amountSimPushed > 0) {
+                to.receiveEnergy(from.extractEnergy(amountSimPushed, false), false);
+            }
+        }
     }
 
     @Override
     protected LazyOptional<IEnergyStorage> getInternalCAP() {
-        return null;
+        return lazyEnergy;
     }
 
     @Override
     protected LazyOptional<IEnergyStorage> createOutputCAP() {
-        return null;
+        return LazyOptional.of(() -> new EnergyStorageDirectional(energyComponent, false));
     }
 
     @Override
     protected LazyOptional<IEnergyStorage> createInputCAP() {
-        return null;
+        return LazyOptional.of(() -> new EnergyStorageDirectional(energyComponent, true));
+    }
+
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+        super.deserializeNBT(nbt);
+        this.energyComponent.deserializeNBT(nbt.getCompound("energy"));
+    }
+
+    @Override
+    public CompoundNBT serializeNBT() {
+        CompoundNBT compoundNBT = super.serializeNBT();
+        compoundNBT.put("energy", energyComponent.serializeNBT());
+        return compoundNBT;
     }
 
     @Override
     public List<IFactory<? extends IScreenAddon>> getScreenAddons() {
-        return null;
+        return energyComponent.getScreenAddons();
     }
 }
--- src/main/java/xyz/brassgoggledcoders/transport/tileentity/loader/FluidLoaderTileEntity.java
+++ src/main/java/xyz/brassgoggledcoders/transport/tileentity/loader/FluidLoaderTileEntity.java
@@ -2,16 +2,24 @@ package xyz.brassgoggledcoders.transport.tileentity.loader;
 
 import com.hrznstudio.titanium.api.IFactory;
 import com.hrznstudio.titanium.api.client.IScreenAddon;
+import com.hrznstudio.titanium.component.fluid.FluidTankComponent;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandler;
+import xyz.brassgoggledcoders.transport.capability.FluidHandlerDirectional;
 import xyz.brassgoggledcoders.transport.content.TransportBlocks;
 
 import java.util.List;
 
 public class FluidLoaderTileEntity extends BasicLoaderTileEntity<IFluidHandler> {
+    private final FluidTankComponent<FluidLoaderTileEntity> fluidTankComponent;
+    private final LazyOptional<IFluidHandler> lazyFluid;
+
     public FluidLoaderTileEntity() {
         super(TransportBlocks.FLUID_LOADER.getTileEntityType(), CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY);
+        this.fluidTankComponent = new FluidTankComponent<>("Loader", 10000, 50, 24);
+        this.lazyFluid = LazyOptional.of(() -> fluidTankComponent);
     }
 
     @Override
@@ -21,21 +29,34 @@ public class FluidLoaderTileEntity extends BasicLoaderTileEntity<IFluidHandler> 
 
     @Override
     protected LazyOptional<IFluidHandler> getInternalCAP() {
-        return null;
+        return lazyFluid;
     }
 
     @Override
     protected LazyOptional<IFluidHandler> createOutputCAP() {
-        return null;
+        return LazyOptional.of(() -> new FluidHandlerDirectional(fluidTankComponent, false));
     }
 
     @Override
     protected LazyOptional<IFluidHandler> createInputCAP() {
-        return null;
+        return LazyOptional.of(() -> new FluidHandlerDirectional(fluidTankComponent, true));
     }
 
     @Override
     public List<IFactory<? extends IScreenAddon>> getScreenAddons() {
-        return null;
+        return fluidTankComponent.getScreenAddons();
+    }
+
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+        super.deserializeNBT(nbt);
+        fluidTankComponent.readFromNBT(nbt.getCompound("tank"));
+    }
+
+    @Override
+    public CompoundNBT serializeNBT() {
+        CompoundNBT nbt = super.serializeNBT();
+        nbt.put("tank", fluidTankComponent.writeToNBT(new CompoundNBT()));
+        return nbt;
     }
 }
