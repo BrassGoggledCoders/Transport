7909376ac8311a98cf19243928ccc763a56e92a0
Skylar <skylar.som@gmail.com>
2020-02-29 13:04:31 -0500

    beginning of datagens

--- src/main/java/xyz/brassgoggledcoders/transport/provider/TransportDataGenerator.java
+++ src/main/java/xyz/brassgoggledcoders/transport/provider/TransportDataGenerator.java
@@ -1,10 +1,15 @@
 package xyz.brassgoggledcoders.transport.provider;
 
+import net.minecraft.data.DataGenerator;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
 public class TransportDataGenerator {
 
     public static void gather(GatherDataEvent event) {
-        new TransportRecipeProvider(event.getGenerator());
+        DataGenerator dataGenerator = event.getGenerator();
+
+        if (event.includeServer()) {
+            dataGenerator.addProvider(new TransportRecipeProvider(dataGenerator));
+        }
     }
 }
--- src/main/java/xyz/brassgoggledcoders/transport/provider/TransportRecipeProvider.java
+++ src/main/java/xyz/brassgoggledcoders/transport/provider/TransportRecipeProvider.java
@@ -1,10 +1,13 @@
 package xyz.brassgoggledcoders.transport.provider;
 
 import net.minecraft.data.*;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.item.crafting.ShapelessRecipe;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.IItemProvider;
+import net.minecraftforge.common.Tags;
 import xyz.brassgoggledcoders.transport.content.TransportBlocks;
 
 import javax.annotation.Nonnull;
@@ -32,11 +35,36 @@ public class TransportRecipeProvider extends RecipeProvider {
                 .addCriterion("has_rail", this.hasItem(ItemTags.RAILS))
                 .build(consumer);
         //endregion
+
+        //region Loaders
+        createLoader(TransportBlocks.ITEM_LOADER.getItem(), Tags.Items.CHESTS)
+                .build(consumer);
+        createLoader(TransportBlocks.ENERGY_LOADER.getItem(), Tags.Items.DUSTS_REDSTONE)
+                .build(consumer);
+        createLoader(TransportBlocks.FLUID_LOADER.getItem(), Ingredient.fromItems(Items.BUCKET))
+                .addCriterion("has_item", this.hasItem(Items.BUCKET))
+                .build(consumer);
+        //endregion
     }
 
     @Override
     @Nonnull
     public String getName() {
         return "Transport Recipes";
+    }
+
+    public ShapedRecipeBuilder createLoader(IItemProvider loader, Tag<Item> center) {
+        return createLoader(loader, Ingredient.fromTag(center))
+                .addCriterion("has_item", this.hasItem(center));
+    }
+
+    public ShapedRecipeBuilder createLoader(IItemProvider loader, Ingredient center) {
+        return ShapedRecipeBuilder.shapedRecipe(loader)
+                .patternLine("III")
+                .patternLine("PCP")
+                .patternLine("III")
+                .key('I', Tags.Items.INGOTS_IRON)
+                .key('P', ItemTags.PLANKS)
+                .key('C', center);
     }
 }
