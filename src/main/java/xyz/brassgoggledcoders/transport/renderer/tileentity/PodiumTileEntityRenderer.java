97beb90238d56c38b186da6ae23e3fa34f57fc82
Skylar <skylar.som@gmail.com>
2020-08-28 21:36:37 -0400

    Podium now renders items put into it.

--- src/main/java/xyz/brassgoggledcoders/transport/renderer/tileentity/PodiumTileEntityRenderer.java
+++ src/main/java/xyz/brassgoggledcoders/transport/renderer/tileentity/PodiumTileEntityRenderer.java
@@ -3,12 +3,15 @@ package xyz.brassgoggledcoders.transport.renderer.tileentity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.model.BookModel;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.EnchantmentTableTileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.util.math.vector.Quaternion;
 import net.minecraft.util.math.vector.Vector3f;
 import xyz.brassgoggledcoders.transport.block.PodiumBlock;
 import xyz.brassgoggledcoders.transport.tileentity.PodiumTileEntity;
@@ -40,6 +43,18 @@ public class PodiumTileEntityRenderer extends TileEntityRenderer<PodiumTileEntit
                         RenderType::getEntitySolid);
                 this.bookModel.renderAll(matrixStack, ivertexbuilder, combinedLight, combinedOverlay, 1.0F,
                         1.0F, 1.0F, 1.0F);
+                matrixStack.pop();
+            } else if (!tileEntity.getDisplayItemStack().isEmpty()) {
+                matrixStack.push();
+                matrixStack.translate(0.5D, 0.9775D, 0.7D);
+                float f = blockstate.get(PodiumBlock.FACING).rotateY().getHorizontalAngle();
+                matrixStack.rotate(Vector3f.YP.rotationDegrees(-f));
+                matrixStack.rotate(Vector3f.ZP.rotationDegrees(67.5F));
+                matrixStack.translate(0.0D, -0.125D, 0.0D);
+                matrixStack.rotate(new Quaternion(new Vector3f(0F, 1F, 0F), 90, true));
+                Minecraft.getInstance().getItemRenderer().renderItem(tileEntity.getDisplayItemStack(),
+                        ItemCameraTransforms.TransformType.GROUND, combinedLight, combinedOverlay, matrixStack,
+                        buffer);
                 matrixStack.pop();
             }
         }
