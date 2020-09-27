e48fcb35079037cd68a1dcbb0ec6239b91cb10f3
Skylar <skylar.som@gmail.com>
2020-09-26 22:42:39 -0400

    Better handling of invalidation for providers

--- src/main/java/xyz/brassgoggledcoders/transport/event/EventHandler.java
+++ src/main/java/xyz/brassgoggledcoders/transport/event/EventHandler.java
@@ -8,30 +8,38 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import xyz.brassgoggledcoders.transport.Transport;
 import xyz.brassgoggledcoders.transport.api.TransportAPI;
+import xyz.brassgoggledcoders.transport.api.master.IManageable;
 import xyz.brassgoggledcoders.transport.api.master.Manageable;
+import xyz.brassgoggledcoders.transport.api.predicate.PredicateStorage;
 import xyz.brassgoggledcoders.transport.api.predicate.PredicateStorageProvider;
 import xyz.brassgoggledcoders.transport.capability.NBTCapabilityProvider;
 import xyz.brassgoggledcoders.transport.capability.itemhandler.furnaceminecart.FurnaceMinecartFuelProvider;
 
 public class EventHandler {
     public static final ResourceLocation MANAGEABLE = Transport.rl("manageable");
-
+    public static final ResourceLocation FURNACE_FUEL = Transport.rl("furnace_fuel");
+    public static final ResourceLocation PREDICATE_STORAGE = Transport.rl("predicate_storage");
     public static void onAttachEntityCapabilities(AttachCapabilitiesEvent<Entity> entityAttachCapabilitiesEvent) {
         if (entityAttachCapabilitiesEvent.getObject() instanceof FurnaceMinecartEntity) {
-            entityAttachCapabilitiesEvent.addCapability(new ResourceLocation(Transport.ID, "furnace_fuel"),
-                    new FurnaceMinecartFuelProvider((FurnaceMinecartEntity) entityAttachCapabilitiesEvent.getObject()));
+            FurnaceMinecartFuelProvider fuelProvider =  new FurnaceMinecartFuelProvider(
+                    (FurnaceMinecartEntity) entityAttachCapabilitiesEvent.getObject());
+            entityAttachCapabilitiesEvent.addCapability(FURNACE_FUEL, fuelProvider);
+            entityAttachCapabilitiesEvent.addListener(fuelProvider::invalidate);
         }
     }
 
     public static void onAttachTileEntityCapabilities(AttachCapabilitiesEvent<TileEntity> attachCapabilitiesEvent) {
         if (attachCapabilitiesEvent.getObject() instanceof LecternTileEntity) {
-            attachCapabilitiesEvent.addCapability(new ResourceLocation(Transport.ID, "predicate_storage"),
-                    new PredicateStorageProvider());
+            PredicateStorageProvider storageProvider = new PredicateStorageProvider();
+            attachCapabilitiesEvent.addCapability(PREDICATE_STORAGE, storageProvider);
+            attachCapabilitiesEvent.addListener(storageProvider::invalidate);
         }
 
         if (!attachCapabilitiesEvent.getCapabilities().containsKey(MANAGEABLE)) {
-            attachCapabilitiesEvent.addCapability(MANAGEABLE, new NBTCapabilityProvider<>(TransportAPI.MANAGEABLE,
-                    new Manageable(null)));
+            NBTCapabilityProvider<IManageable> manageableProvider = new NBTCapabilityProvider<>(TransportAPI.MANAGEABLE,
+                    new Manageable(null));
+            attachCapabilitiesEvent.addCapability(MANAGEABLE, manageableProvider);
+            attachCapabilitiesEvent.addListener(manageableProvider::invalidate);
         }
     }
 }
