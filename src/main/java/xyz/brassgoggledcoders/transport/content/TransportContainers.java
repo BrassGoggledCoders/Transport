64388e4e50fa90be609315cb70ff1e9dcc009918
Skylar <skylar.som@gmail.com>
2020-12-24 12:38:18 -0500

    Fix error about not being to figure which method ot use.

--- src/main/java/xyz/brassgoggledcoders/transport/content/TransportContainers.java
+++ src/main/java/xyz/brassgoggledcoders/transport/content/TransportContainers.java
@@ -5,6 +5,7 @@ import com.hrznstudio.titanium.container.BasicAddonContainer;
 import com.hrznstudio.titanium.network.locator.LocatorFactory;
 import com.hrznstudio.titanium.network.locator.LocatorInstance;
 import com.hrznstudio.titanium.network.locator.instance.EmptyLocatorInstance;
+import com.tterrag.registrate.builders.ContainerBuilder;
 import com.tterrag.registrate.util.entry.RegistryEntry;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -12,7 +13,6 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.IWorldPosCallable;
-import net.minecraft.util.Util;
 import net.minecraft.world.World;
 import net.minecraftforge.common.extensions.IForgeContainerType;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -27,6 +27,9 @@ import xyz.brassgoggledcoders.transport.api.module.ModuleType;
 import xyz.brassgoggledcoders.transport.container.EntityLocatorInstance;
 import xyz.brassgoggledcoders.transport.container.navigation.NavigationChartContainer;
 import xyz.brassgoggledcoders.transport.screen.navigation.NavigationChartScreen;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
 public class TransportContainers {
     private static final DeferredRegister<ContainerType<?>> CONTAINERS =
@@ -82,9 +85,17 @@ public class TransportContainers {
     public static final RegistryEntry<ContainerType<NavigationChartContainer>> NAVIGATION_CHART =
             Transport.getRegistrate()
                     .object("navigation_chart")
-                    .container(NavigationChartContainer::create, () -> NavigationChartScreen::new)
+                    .container(new ContainerBuilder.ContainerFactory<NavigationChartContainer>() {
+                        @Override
+                        @Nonnull
+                        @ParametersAreNonnullByDefault
+                        public NavigationChartContainer create(ContainerType<NavigationChartContainer> containerType,
+                                                               int i, PlayerInventory playerInventory) {
+                            return new NavigationChartContainer(containerType, i);
+                        }
+                    }, () -> NavigationChartScreen::new)
                     .register();
-    
+
     public static void register(IEventBus eventBus) {
         CONTAINERS.register(eventBus);
     }
