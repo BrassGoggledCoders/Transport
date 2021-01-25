cb356246f34c14e3d8b270d385ecc226f142018e
Andreas <andreas.sch4@gmail.com>
2021-01-25 19:21:02 +0100

    Revert "Add Regex to NamePredicate"
    This reverts commit e0a62f2b

--- src/main/java/xyz/brassgoggledcoders/transport/predicate/NamePredicate.java
+++ src/main/java/xyz/brassgoggledcoders/transport/predicate/NamePredicate.java
@@ -6,8 +6,6 @@ import xyz.brassgoggledcoders.transport.api.predicate.PredicateParser;
 import xyz.brassgoggledcoders.transport.api.predicate.PredicateParserException;
 
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
 public class NamePredicate implements Predicate<Entity> {
     private final Predicate<String> checker;
@@ -25,19 +23,8 @@ public class NamePredicate implements Predicate<Entity> {
     public static NamePredicate create(PredicateParser predicateParser) throws PredicateParserException {
         if (predicateParser.hasNextString()) {
             return new NamePredicate(predicateParser.getNextString()::equals);
-        } else if (predicateParser.hasNextRegex()) {
-            String regex = predicateParser.getNextRegex();
-            return new NamePredicate(s -> {
-                try {
-                    return Pattern.matches(regex, s);
-                } catch (PatternSyntaxException e) {
-                    return false;
-                }
-            });
         } else {
             return new NamePredicate(predicateParser.getNextStringPredicate());
         }
     }
-
-
 }
