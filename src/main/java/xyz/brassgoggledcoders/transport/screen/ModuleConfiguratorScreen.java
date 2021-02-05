dabb4ac4b830e70e851a14748fd51b1aca3e46b4
Skylar <skylar.som@gmail.com>
2021-02-04 21:46:57 -0500

    Module Configurator Screen now shows Tooltips

--- src/main/java/xyz/brassgoggledcoders/transport/screen/ModuleConfiguratorScreen.java
+++ src/main/java/xyz/brassgoggledcoders/transport/screen/ModuleConfiguratorScreen.java
@@ -59,5 +59,6 @@ public class ModuleConfiguratorScreen extends ContainerScreen<ModuleConfigurator
     public void render(@Nonnull MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
         this.renderBackground(matrixStack);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
+        this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
     }
 }
