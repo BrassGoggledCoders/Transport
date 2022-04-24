059a473cc559c07baa4f844951cd08a33876b3b0
Skylar <skylar.som@gmail.com>
2022-04-23 23:58:24 -0400

    rail worker bench done

---
+++ src/main/java/xyz/brassgoggledcoders/transport/compat/jei/TransportJEI.java
@@ -0,0 +1,50 @@
+package xyz.brassgoggledcoders.transport.compat.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import xyz.brassgoggledcoders.transport.Transport;
+import xyz.brassgoggledcoders.transport.content.TransportBlocks;
+import xyz.brassgoggledcoders.transport.content.TransportRecipes;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+@JeiPlugin
+public class TransportJEI implements IModPlugin {
+    private static final ResourceLocation PLUGIN_UID = Transport.rl("jei");
+
+    @Override
+    @Nonnull
+    public ResourceLocation getPluginUid() {
+        return PLUGIN_UID;
+    }
+
+    @Override
+    public void registerCategories(@Nonnull IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new RailWorkerBenchCategory(registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(@Nonnull IRecipeRegistration registration) {
+        registration.addRecipes(
+                RailWorkerBenchCategory.JEI_RECIPE_TYPE,
+                Objects.requireNonNull(Minecraft.getInstance().level)
+                        .getRecipeManager()
+                        .getAllRecipesFor(TransportRecipes.RAIL_WORKER_BENCH_TYPE.get())
+        );
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(
+                new ItemStack(TransportBlocks.RAIL_WORKER_BENCH.get()),
+                RailWorkerBenchCategory.JEI_RECIPE_TYPE
+        );
+    }
+}
