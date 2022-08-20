5462648e158487076b8457f6c820708cf168d11e
Skylar <skylar.som@gmail.com>
2022-08-20 17:14:16 -0400

    A block above a sloped copper rail now stops the cart

--- Transport/src/main/java/xyz/brassgoggledcoders/transport/block/rail/CopperRail.java
+++ Transport/src/main/java/xyz/brassgoggledcoders/transport/block/rail/CopperRail.java
@@ -1,10 +1,13 @@
 package xyz.brassgoggledcoders.transport.block.rail;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.RailBlock;
+import net.minecraft.world.level.block.SupportType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 
 public class CopperRail extends RailBlock {
     public CopperRail(Properties properties) {
@@ -18,6 +21,12 @@ public class CopperRail extends RailBlock {
 
     @Override
     public void onMinecartPass(BlockState state, Level level, BlockPos pos, AbstractMinecart cart) {
-        cart.setDeltaMovement(cart.getDeltaMovement().scale(1.2));
+        if (state.getValue(this.getShapeProperty()).isAscending() &&
+                level.getBlockState(pos.above()).isFaceSturdy(level, pos, Direction.DOWN, SupportType.RIGID)) {
+            cart.setDeltaMovement(Vec3.ZERO);
+        } else {
+            cart.setDeltaMovement(cart.getDeltaMovement().scale(1.2));
+        }
+
     }
 }
