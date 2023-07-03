12a71efdea57aa4b76da7d32c159197d5ff1d112
Skylar <skylar.som@gmail.com>
2023-07-03 16:12:26 -0400

    Delete the old module shit, move to 1.19.2

--- TransportFriends/src/main/java/xyz/brassgoggledcoders/transport/friends/TransportFriends.java
+++
@@ -1,8 +0,0 @@
-package xyz.brassgoggledcoders.transport.friends;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(TransportFriends.ID)
-public class TransportFriends {
-    public static final String ID = "transport_friends";
-}
